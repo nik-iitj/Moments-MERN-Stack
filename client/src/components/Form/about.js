7fc3269fd45160810d44773b9a45b2075a60191f
nik-iitj <raj.14@iitj.ac.in>
2021-07-19 16:16:08 +0530

    Moments final build

---
+++ client/src/components/Form/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {textField,Button,Typography,Paper, TextField, Box} from '@material-ui/core';
+const about = () => {
+    return (
+        <>
+            Helllloo
+        </>
+    )
+}
+
+export default about;
